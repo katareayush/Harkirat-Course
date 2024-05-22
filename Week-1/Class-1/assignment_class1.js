30a6cf606ae4877f4ca9a5322ce76c1be0371f08
Ayush Katare <katareayush2005@gmail.com>
2024-05-23 00:18:18 +0530

    JS FOUNDATION over

---
+++ Week-1/Class-1/assignment_class1.js
@@ -0,0 +1,34 @@
+//COUNTER
+
+// let counter = 30;
+
+// for (let i = counter; i > 0; i--) {
+//     console.log(i);
+// }
+
+
+// TIME
+
+// function time() {   
+// console.log("Hello World")
+// }
+
+// setTimeout(time, 1000);
+
+function printTime() {
+const currentTime = new Date();
+const hours = String(currentTime.getHours()).padStart(2, '0');
+const minutes = String(currentTime.getMinutes()).padStart(2, '0');
+const seconds = String(currentTime.getSeconds()).padStart(2, '0');
+const timeString = `${hours}:${minutes}:${seconds}`;
+
+console.log(timeString);
+}
+
+function startClock() {
+printTime();
+setInterval(printTime, 1000);
+}
+
+startClock();
+
