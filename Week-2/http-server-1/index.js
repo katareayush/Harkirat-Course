2b1e1348ed077932eaa5f48f72a36c799028fc51
Ayush Katare <katareayush2005@gmail.com>
2024-05-24 16:28:03 +0530

    express basic and HTTP basics

---
+++ Week-2/http-server-1/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', function(req, res)  {
+  res.send('Hello World!')
+})
+app.get('/route-handler', function(req, res)  {
+  res.send('Hello World! from route handler')
+})
+
+app.listen(port)
