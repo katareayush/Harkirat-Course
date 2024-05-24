ca80b858aa496cc97c711608524afbb0ad0244bc
Ayush Katare <katareayush2005@gmail.com>
2024-05-25 04:44:34 +0530

    filter map and arrow function done

--- Week-1/async-sync-fns.js
+++ Week-1/async-sync-fns.js
@@ -1,8 +1,44 @@
-const fs = require ("fs");
-// filesystem module
+// const fs = require ("fs");
+// // filesystem module
 
-fs.readFile("a.txt","utf-8", function(err, data){
-    console.log(data);
-})
+// fs.readFile("a.txt","utf-8", function(err, data){
+//     console.log(data);
+// })
 
-console.log("This is the last line of the code");
+// console.log("This is the last line of the code");
+
+
+//ARROW FUNCTION
+// const sum = (a,b) =>{
+//     return a+b;
+// }
+
+// const ans = sum(2,3);
+// console.log(ans);
+
+//MAP FUNCTION
+// const input= [1,2,3,4,5];
+
+// function double(n){
+//     return n*2;
+// }
+
+// const ans=input.map(double);
+// console.log(ans);
+
+//FILTER FUNCTION
+
+const arr=[1,2,3,4,5];
+
+function filterlogic(n)
+{
+    if(n%2==0){
+        return true;
+    }
+    else{
+        return false;
+    }   
+}
+
+const ans=arr.filter(filterlogic);
+console.log(ans);
