e478df54617da8557d4240af25262d04b4f10652
Ayush Katare <katareayush2005@gmail.com>
2024-06-21 16:33:31 +0530

    Tailwind Basics Completed

--- Week-8/8.2/components/RevenueCard.jsx
+++ Week-8/8.2/components/RevenueCard.jsx
@@ -1,3 +1,4 @@
+import { Button } from "@mui/material"
 
 export const RevenueCard = ({
     title,
@@ -5,9 +6,12 @@ export const RevenueCard = ({
     amount
 }) => {
     return (
-    <div className="bg-white rounded shadow-md p-4">
-        <div className="text-gray-700 font-semibold">
-            {title}
+    <div className="bg-white rounded shadow-md p-4  cursor-pointer">
+        <div className="flex text-gray-700 font-semibold hover:bg-red-100">
+            
+            {title} <div className="flex ml-1 mt-1">{<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z" />
+            </svg>}</div>
         </div>
 
         <div className="flex justify-between font-bold ">
--- Week-8/8.2/src/App.jsx
+++ Week-8/8.2/src/App.jsx
@@ -4,7 +4,7 @@ import './App.css'
 function App() {
   return (
    <div className='grid grid-cols-4'>
-    <RevenueCard title="Amount Pending?" amount="92,310.20" orderCount={13} />
+    <RevenueCard title="Amount Pending" amount="92,310.20" orderCount={13} />
    </div>
   )
 } 
